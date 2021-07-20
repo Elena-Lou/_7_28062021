a3423af1be6a20558129559f3d4769ef9208f917
Elena_Lou <elouisiade@live.fr>
2021-07-20 23:30:21 +0200

    modif Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,32 +1,19 @@
 <template>
-  <div class="home">
-      <img src="../assets/icon.png" alt="logo Groupomania">
-    <Posts/>
-    
-    <NewPost/>
 
-  </div>
+  <div class="home">
+    <h1>Bienvenue</h1>
+
+        <div class="router">
+          <router-link to="/login">Connexion</router-link>
+          <router-link to="/signup">Inscription</router-link>
+        </div>
+
+    </div>
+
 </template>
 
 <script>
-
-import Posts from "../components/Posts";
-import NewPost from "../components/NewPost";
-
 export default {
-  name: "Home",
-  components: {
-    Posts,
-    NewPost,
-  },
-  
+  name: "Home"
 }
 </script>
-
-<style lang="scss" scoped>
-img {
-  width: 30%;
-  margin: auto;
-  align-self: center;
-}
-</style>
