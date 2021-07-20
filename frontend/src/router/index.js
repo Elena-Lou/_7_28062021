a230c5a5bb3acde5eb7f2c60ed600f09e2c26ffd
Elena_Lou <elouisiade@live.fr>
2021-07-20 14:17:15 +0200

    modification du router une page par élément

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,58 +1,89 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import Signup from "../views/Signup.vue";
-import Post from "../views/SinglePost.vue";
-import Profile from "../views/Profile.vue";
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Posts from "../views/Posts.vue";
+import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import Signup from "../views/Signup.vue";
+import SinglePost from "../views/SinglePost.vue";
+import NewPost from "../views/NewPost.vue";
+import Profile from "../views/Profile.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
+Vue.use(VueRouter);
 
-  {
-    path: "/login",
-    name: "login",
-    component: Login,
-  },
-
-  {
-    path: "/signup",
-    name: "signup",
-    component: Signup,
-  },
-
-  {
-    path: "/posts/:id",
-    name: "Post",
-    component: Post,
-  },
-
-  {
-    path: "/user/:id",
-    name: "Profile",
-    component: Profile,
-  },
-];
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+      beforeEnter: (to, from, next) => {
+        const token = localStorage.getItem("userToken");
+        if (!token) {
+          next("/login");
+        }
+        next();
+      },
+    },
+    {
+      path: "/posts",
+      name: "Posts",
+      component: Posts,
+      beforeEnter: (to, from, next) => {
+        const token = localStorage.getItem("userToken");
+        if (!token) {
+          next("/login");
+        }
+        next();
+      },
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/signup",
+      name: "Signup",
+      component: Signup,
+    },
+    {
+      path: "/posts/:id",
+      name: "SinglePost",
+      component: SinglePost,
+      beforeEnter: (to, from, next) => {
+        const token = localStorage.getItem("userToken");
+        if (!token) {
+          next("/login");
+        }
+        next();
+      },
+    },
+    {
+      path: "/post",
+      name: "NewPost",
+      component: NewPost,
+      beforeEnter: (to, from, next) => {
+        const token = localStorage.getItem("userToken");
+        if (!token) {
+          next("/login");
+        }
+        next();
+      },
+    },
+    {
+      path: "/user/:id",
+      name: "Profile",
+      component: Profile,
+      beforeEnter: (to, from, next) => {
+        const token = localStorage.getItem("userToken");
+        if (!token) {
+          next("/login");
+        }
+        next();
+      },
+    },
+  ],
 });
-
-// router.beforeEach((to, from, next) => {
-//   const publicPages = ["/login", "/signup"];
-//   const authRequired = !publicPages.includes(to.path);
-//   const isLoggedIn = this.$store.getters.isLoggedIn
-
-
-//   if (authRequired && !isLoggedIn) {
-//     next("/login");
-//   } else {
-//     next();
-//   }
-// });
 
 export default router;
