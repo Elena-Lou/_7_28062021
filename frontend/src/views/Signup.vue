bbc58fdc43cddb2fbba2558389f180f1f1614541
Elena_Lou <elouisiade@live.fr>
2021-07-20 23:15:48 +0200

    modif <signup intégré signup.vue

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,20 +1,64 @@
 <template>
   <div class="signup">
-      <h2>Bienvenue !</h2>
-        <p>Veuillez renseigner les informations ci-dessous pour compléter votre profil et commencer à échanger avec vos collègues</p>
-    <SignupForm/>
-  </div>
+
+        <form class="signup__form">
+
+            <label for="name">Vous êtes :</label>
+            <input v-model.trim="name" id="name" type="text" placeholder="Albert Camus" required/>
+
+            <label for="email">Votre adresse email :</label>
+            <input name="email" v-model.trim="email" id="email" type="email" placeholder="albertcamus@groupomania.com" required/> 
+
+            <label for="password">Votre mot de passe :</label>
+            <input name="password" v-model="password" id="password" type="text" placeholder="Mot de passe" required/>
+
+            <button id="btn__signup" type="submit" @click.prevent="signup">S'inscrire</button>
+
+            <div class="btn-signup-page">
+            <router-link to="/login" class="connexion btn btn-secondary mx-5" title="Se connecter">Vous avez déjà un compte ? Connectez-vous !</router-link>
+            </div>
+
+        </form>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import SignupForm from '../components/SignupForm.vue';
+
+import UserServices from "../services/user-services.js"
 
 export default {
   name: 'Signup',
-  components: {
-    SignupForm
-  }
 
+ data() {
+    return {
+      name:"",
+      email: "",
+      password: "",
+    };
+  },
+
+  methods: {
+    signup() {
+        UserServices.signup({
+          name: this.name,
+          email: this.email,
+          password: this.password
+        })
+          
+          .then(res => {
+            if (res.status === 201) {
+            console.log(res.data.message);
+            this.$router.push("/login");
+            } else {
+              console.log("erreur");
+            }
+          })
+
+      .catch(e => {
+        console.log(e);
+      })    
+    }
+  }
 }
+
 </script>
