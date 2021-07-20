a5c2f69ff74fe4e4ec99dc91b61aa4df8b50b3fd
Elena_Lou <elouisiade@live.fr>
2021-07-20 14:52:44 +0200

    modif login form intégré a login.vue

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,24 +1,59 @@
 <template>
 
-    <div class="login">
-            
-        <h2>Connectez-vous</h2>
+    <div class="login__form">
+     
+        <form class="login__form">
 
-            <p>Veuillez renseigner vos identifiants de connexion</p>
+            <label for="email">Votre adresse email :</label>
+            <input name="email" v-model="email" id="email" type="email" placeholder="albertcamus@groupomania.com" required> 
 
-            <LoginForm/>
+            <label for="password">Votre mot de passe :</label>
+            <input name="password" v-model="password" id="password" type="text" placeholder="Mot de passe" required/>
 
+            <button id="btn__login" @click.prevent="login" type="submit">Se connecter</button>
+
+        </form>
+
+            <div class="signup__link">
+                <p>Vous n'avez pas encore de compte ?
+                  <router-link to="/signup" id="signup" title="Créer un compte">S'inscrire</router-link>
+                </p>
+            </div>
     </div>
 
 </template>
 
 <script>
-import LoginForm from "../components/LoginForm.vue"
-
+import UserService from "../services/user-services";  
+               
 export default {
-  name: 'Login',
-  components: {
-    LoginForm
-  },
+    name: "Login",
+    data() {
+        return {
+          email : "",
+          password : ""
+        }
+    },
+    mounted() {                                       
+      localStorage.removeItem('userToken');    
+    },
+  methods: {
+    login() {
+        UserService.login({
+          email: this.email,
+          password: this.password
+        })
+      .then((res) => {
+        localStorage.setItem('userToken', res.data.token),   
+        this.$router.push('/posts');                          
+        location.reload();
+      })
+      .catch((e) => {
+        if (e.response.status === 401) {
+        this.message = "E-mail ou mot de passe incorrect";
+        }
+      })
+    }
+  }
 }
 </script>
